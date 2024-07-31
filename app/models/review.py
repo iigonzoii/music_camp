e34ea512e2999bc4954be2a14cb68b22d4c3e07e
iigonzoii <a.gonzalez418@gmail.com>
2024-07-31 18:55:29 -0500

    smallfix

--- app/models/review.py
+++ app/models/review.py
@@ -1,23 +1,18 @@
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import Column, Integer, String, Float, ForeignKey
-from sqlalchemy.orm import relationship
-
-db = SQLAlchemy()
+from .db import db, SCHEMA, environment
 
 class Review(db.Model):
     __tablename__ = 'reviews'
 
-    # if environment == "production":
-    # __table_args__ = {'schema': SCHEMA}
+    if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
     id = db.Column(db.Integer, primary_key = True)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
     album_id = db.Column(db.Integer, db.ForeignKey('albums.id'), nullable=False)
     review = db.Column(db.String(250), nullable=False)
     stars = db.Column(db.Integer, nullable=False)
-    #! might not need created and updated
     created_at = db.Column(db.Date)
     updated_at = db.Column(db.Date)
     #* relationship to user
-    reviewer = relationship('User', back_populates='reviews')
+    reviewer = db.relationship('User', back_populates='reviews')
     #* relationship to album
-    album = relationship('Album', back_populates='reviews')
+    album = db.relationship('Album', back_populates='reviews')

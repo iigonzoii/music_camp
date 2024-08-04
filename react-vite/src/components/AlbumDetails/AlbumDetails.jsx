7197f148235d6c2c28cec8bdb92dc4f8f3289c84
iigonzoii <a.gonzalez418@gmail.com>
2024-08-03 20:07:28 -0500

    refactoring album details into more components

--- react-vite/src/components/AlbumDetails/AlbumDetails.jsx
+++ react-vite/src/components/AlbumDetails/AlbumDetails.jsx
@@ -1,54 +1,40 @@
+import MajorityDetails from "./MajorityDetails"
 import "./AlbumDetails.css"
+
 // todo top container
-    //*
+//*
 // todo left container
-    //* two inner containers labeled with data of some sort
-    //* within each sub container create element placeholders for said data
+//* two inner containers labeled with data of some sort
+//* within each sub container create element placeholders for said data
 // todo right container
-    //* flex column and seperate data groups into divs
+//* flex column and seperate data groups into divs
 // todo top container element placement
 // todo left container inner container
 
 function AlbumDetails() {
 
     return (
-        <>
-        <section className="ADsection1">
-ay ay shawtayyyy
-        </section>
+        <div className="ADbody">
+            <section className="ADsection1">
 
-        <div className="ADcontainer">
-            <section className="ADsection2">
-
-                <div className="ADalbumData">
-                    sub container left
-                    <p>album title</p>
-                    <p>by user.username</p>
-                    <div className="V2Play">version2 play button and song</div>
-                    <p>Product Type</p>
-                    <button>Buy Product.Type</button>
-                    <p>album.description</p>
-
-                    <ol className="ADtrackList">
-                        <li className="test">trackname track duration</li>
-                        <li className="test">trackname track duration</li>
-                        <li className="test">trackname track duration</li>
-                    </ol>
-                    <p>produced by</p>
-                    <p>Released `Release Date`</p>
-                </div>
-
-                <div className="ADalbumArea">
-                sub container right
-                </div>
+                    <img className="ADbanner" src="https://firebasestorage.googleapis.com/v0/b/musiccamp-88aaa.appspot.com/o/musicCampBackground.jpg?alt=media&token=df57940f-056e-468f-aae7-dfe860753847"/>
 
             </section>
 
-            <aside className="ADsection3">
-section 3
-            </aside>
+            <div className="ADcontainer">
+                <section className="ADsection2">
+                    <MajorityDetails />
+                </section>
+
+                <aside className="ADsection3">
+                    section 3
+                  <img className="ADasideImg" src="https://firebasestorage.googleapis.com/v0/b/musiccamp-88aaa.appspot.com/o/musicCampUserProfileImg.jpg?alt=media&token=949d5249-d3c3-4e79-a385-4f3e0774c6bc"/>
+                  <div></div>
+                  <div></div>
+                  <div></div>
+                </aside>
+            </div>
         </div>
-        </>
     )
 }
 
---
+++ react-vite/src/components/AlbumDetails/MajorityDetails.jsx
@@ -0,0 +1,46 @@
+import "./AlbumDetails.css"
+function MajorityDetails() {
+
+    return (
+        <>
+        <div className="ADalbumData">
+                    sub container left
+                    <p>album title</p>
+                    <p>by user.username</p>
+                    <div className="V2Play">version2 play button and song</div>
+                    <p>Product Type</p>
+                    <button>Buy Product.Type</button>
+                    <p>album.description</p>
+
+                    <ol className="ADtrackList">
+                        <li className="test">trackname track duration</li>
+                        <li className="test">trackname track duration</li>
+                        <li className="test">trackname track duration</li>
+                    </ol>
+                    <p>produced by</p>
+                    <p>Released `Release Date`</p>
+                </div>
+
+                <div className="ADalbumArea">
+
+                    <img src="https://firebasestorage.googleapis.com/v0/b/musiccamp-88aaa.appspot.com/o/musicCampRockAlbumCover.jpg?alt=media&token=3d36d4a9-7fab-4f37-ac7d-e677d5a94495" />
+
+                    <p className="test"><i className="fa-regular fa-heart pointer "></i>wishlist</p>
+                    <p className="test">Supported by</p>
+                    <ul>
+                        <li>reviewer profile image and their review</li>
+                        <li>reviewer profile image and their review</li>
+                        <li>reviewer profile image and their review</li>
+                        <li>reviewer profile image and their review</li>
+                        <li>Version 2 probably make this box scroll and seethrough to show the background img?</li>
+                    </ul>
+                    <div className="ADv2supporters">
+                    Version2 where we showed a tiled list of supporters profile images
+                    </div>
+
+                </div>
+        </>
+    )
+}
+
+export default MajorityDetails

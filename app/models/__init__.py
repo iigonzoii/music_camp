3461e092c01e606aebf5171b13aa34a3b9604caa
Brian Wilkinson <brian.joseph.wilkinson@gmail.com>
2024-08-05 07:10:14 -0700

    fixed import

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -2,7 +2,7 @@ from .db import db
 from .user import User
 from .album import Album
 from .track import Track
-from .associations import wishlist, cart_items
+from .associations import wishlist
 from .review import Review
 from .purchase_item import PurchaseItem
 from .db import environment, SCHEMA

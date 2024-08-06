8869788d11527ec0f5de1ce9d26a841a0f82d8c8
iigonzoii <a.gonzalez418@gmail.com>
2024-08-06 13:07:04 -0500

    removing commented out code and console logs

--- react-vite/src/components/AlbumDetails/MajorityDetails.jsx
+++ react-vite/src/components/AlbumDetails/MajorityDetails.jsx
@@ -7,37 +7,20 @@ import "./AlbumDetails.css"
 
 function MajorityDetails() {
     const { albumId } = useParams()
-    console.log("ALBUMIDDDDDD", albumId)
     const dispatch = useDispatch();
     let album = useSelector(state => state.album.albumDetail);
-    console.log("ALBUM", album)
     let [isLoaded, setIsLoaded] = useState(false)
-    // album = Object.values(album)
-    // let reserve = () => alert("Feature coming soon")
-    // let [isLoaded, setIsLoaded] = useState(false)
-    // let review = useSelector(state => state.review)
-    // review = Object.values(review).reverse()
-    // console.log("REVIEWWWW", review)
-    // let userHasReview
-    // if (session.user !== null) userHasReview = review.find(currReview => currReview.userId === session.user.id)
-
-    // useEffect(() => {
-    //     dispatch(fetchReviews(+spotId))
-    //         .then(() => dispatch(fetchSpot(+spotId)))
-    //         .then(() => setIsLoaded(true));
-    //     //! if i get build errors then take this stupid spotId out of here
-    // }, [dispatch, spotId]);
-    useEffect(() => {
-        dispatch(fetchAlbum(+albumId)).then(() =>
-            setIsLoaded(true));
-    }, [dispatch]);
-    if (!album || !album.Album) return
+        useEffect(() => {
+            dispatch(fetchAlbum(+albumId)).then(() =>
+                setIsLoaded(true));
+        }, [dispatch]);
+        if (!album || !album.Album) return
+        console.log("ALBUM", album)
     return isLoaded && (
 
         <>
             <div className="ADalbumData">
                 sub container left
-                {console.log("albumInReturn", album)}
                 <p>{album && album.Album.title}</p>
                 <p>{`by ${album.Album.band}`}</p>
                 <div className="V2Play">version2 play button and song</div>

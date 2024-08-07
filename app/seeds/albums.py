c8cd167fda5606b50005a851aed3cf6e23737863
iigonzoii <a.gonzalez418@gmail.com>
2024-08-07 13:08:37 -0500

    more album and track seeds

--- app/seeds/albums.py
+++ app/seeds/albums.py
@@ -4,19 +4,19 @@ from sqlalchemy.sql import text
 
 def seed_albums():
     album_one = Album(
-        user_id=4, band='ArtistOne', title='albumOne', 
+        user_id=4, band='ArtistOne', title='Artist1Al1',
         cover_image_url='https://firebasestorage.googleapis.com/v0/b/musiccamp-88aaa.appspot.com/o/musicCampRockAlbumCover.jpg?alt=media&token=3d36d4a9-7fab-4f37-ac7d-e677d5a94495',
         description='This is a description about a rock cd',
         producer='awesome producer', genre='rock', tags='hard rock',
     )
     album_two = Album(
-        user_id=5, band='ArtistTwo', title='albumTwo', 
+        user_id=5, band='ArtistTwo', title='Artist2Al1',
         cover_image_url='https://firebasestorage.googleapis.com/v0/b/musiccamp-88aaa.appspot.com/o/musicCampElectronicAlbumCover.jpg?alt=media&token=3f64c61d-5852-4c61-a080-7736041cc47d',
         description='This is a description about a digital album',
         producer='awesome producer', genre='electronic', tags='ambient',
     )
     album_three = Album(
-        user_id=6, band='ArtistThree', title='albumThree', 
+        user_id=6, band='ArtistThree', title='Artist3Al1',
         cover_image_url='https://firebasestorage.googleapis.com/v0/b/musiccamp-88aaa.appspot.com/o/musicCampHipHopVinylCover.jpg?alt=media&token=d379f3e9-15bb-4045-ac4d-fee49461ea35',
         description='This is a description about a vinyl album',
         producer='awesome producer', genre='hip-hop/rap', tags='trap',
@@ -25,7 +25,79 @@ def seed_albums():
         user_id=7, band='ArtistFour', title='albumFour',
         cover_image_url='https://firebasestorage.googleapis.com/v0/b/musiccamp-88aaa.appspot.com/o/musicCampJazzAlbumCover.jpg?alt=media&token=6c3b578d-bc59-4c28-83d7-8e17971608d8',
         description='This is a description about a cassette',
+        producer='awesome producer', genre='jazz', tags='Latin jazz',
+    )
+    album_five= Album(
+        user_id=5, band='ArtistTwo', title='Artist2Al2',
+        cover_image_url='https://firebasestorage.googleapis.com/v0/b/musiccamp-88aaa.appspot.com/o/musicCampJazzAlbumCover.jpg?alt=media&token=6c3b578d-bc59-4c28-83d7-8e17971608d8',
+        description='This is a description about a cassette',
+        producer='awesome producer', genre='rock', tags='Acid rock',
+    )
+    album_six= Album(
+        user_id=5, band='ArtistTwo', title='Artist2Al3',
+        cover_image_url='https://firebasestorage.googleapis.com/v0/b/musiccamp-88aaa.appspot.com/o/musicCampJazzAlbumCover.jpg?alt=media&token=6c3b578d-bc59-4c28-83d7-8e17971608d8',
+        description='This is a description about a cassette',
+        producer='awesome producer', genre='hip-hop/rap', tags='G-funk',
+    )
+    album_seven= Album(
+        user_id=5, band='ArtistTwo', title='Artist2Al4',
+        cover_image_url='https://firebasestorage.googleapis.com/v0/b/musiccamp-88aaa.appspot.com/o/musicCampJazzAlbumCover.jpg?alt=media&token=6c3b578d-bc59-4c28-83d7-8e17971608d8',
+        description='This is a description about a cassette',
+        producer='awesome producer', genre='jazz', tags='Smooth Jazz',
+    )
+    album_eight= Album(
+        user_id=4, band='ArtistOne', title='Artist1Al2',
+        cover_image_url='https://firebasestorage.googleapis.com/v0/b/musiccamp-88aaa.appspot.com/o/musicCampJazzAlbumCover.jpg?alt=media&token=6c3b578d-bc59-4c28-83d7-8e17971608d8',
+        description='This is a description about a cassette',
+        producer='awesome producer', genre='electronic', tags='trap',
+    )
+    album_nine= Album(
+        user_id=4, band='ArtistOne', title='Artist1Al3',
+        cover_image_url='https://firebasestorage.googleapis.com/v0/b/musiccamp-88aaa.appspot.com/o/musicCampJazzAlbumCover.jpg?alt=media&token=6c3b578d-bc59-4c28-83d7-8e17971608d8',
+        description='This is a description about a cassette',
+        producer='awesome producer', genre='hip-hop/rap', tags='Old-school',
+    )
+    album_ten= Album(
+        user_id=4, band='ArtistOne', title='Artist1Al4',
+        cover_image_url='https://firebasestorage.googleapis.com/v0/b/musiccamp-88aaa.appspot.com/o/musicCampJazzAlbumCover.jpg?alt=media&token=6c3b578d-bc59-4c28-83d7-8e17971608d8',
+        description='This is a description about a cassette',
         producer='awesome producer', genre='jazz', tags='electronic',
+    )
+    album_eleven= Album(
+        user_id=6, band='ArtistThree', title='Artist3Al2',
+        cover_image_url='https://firebasestorage.googleapis.com/v0/b/musiccamp-88aaa.appspot.com/o/musicCampJazzAlbumCover.jpg?alt=media&token=6c3b578d-bc59-4c28-83d7-8e17971608d8',
+        description='This is a description about a cassette',
+        producer='awesome producer', genre='rock', tags='Pop-punk',
+    )
+    album_twelve= Album(
+        user_id=6, band='ArtistThree', title='Artist3Al3',
+        cover_image_url='https://firebasestorage.googleapis.com/v0/b/musiccamp-88aaa.appspot.com/o/musicCampJazzAlbumCover.jpg?alt=media&token=6c3b578d-bc59-4c28-83d7-8e17971608d8',
+        description='This is a description about a cassette',
+        producer='awesome producer', genre='jazz', tags='Traditional jazz',
+    )
+    album_thirteen= Album(
+        user_id=6, band='ArtistThree', title='Artist3Al4',
+        cover_image_url='https://firebasestorage.googleapis.com/v0/b/musiccamp-88aaa.appspot.com/o/musicCampJazzAlbumCover.jpg?alt=media&token=6c3b578d-bc59-4c28-83d7-8e17971608d8',
+        description='This is a description about a cassette',
+        producer='awesome producer', genre='electronic', tags='lowfi',
+    )
+    album_fourteen= Album(
+        user_id=7, band='ArtistFour', title='Artist4Al2',
+        cover_image_url='https://firebasestorage.googleapis.com/v0/b/musiccamp-88aaa.appspot.com/o/musicCampJazzAlbumCover.jpg?alt=media&token=6c3b578d-bc59-4c28-83d7-8e17971608d8',
+        description='This is a description about a cassette',
+        producer='awesome producer', genre='rock', tags='Gothic rock',
+    )
+    album_fifteen= Album(
+        user_id=7, band='ArtistFour', title='Artist4Al3',
+        cover_image_url='https://firebasestorage.googleapis.com/v0/b/musiccamp-88aaa.appspot.com/o/musicCampJazzAlbumCover.jpg?alt=media&token=6c3b578d-bc59-4c28-83d7-8e17971608d8',
+        description='This is a description about a cassette',
+        producer='awesome producer', genre='electronic', tags='house',
+    )
+    album_sixteen= Album(
+        user_id=7, band='ArtistFour', title='Artist4Al4',
+        cover_image_url='https://firebasestorage.googleapis.com/v0/b/musiccamp-88aaa.appspot.com/o/musicCampJazzAlbumCover.jpg?alt=media&token=6c3b578d-bc59-4c28-83d7-8e17971608d8',
+        description='This is a description about a cassette',
+        producer='awesome producer', genre='hip-hop/rap', tags='Conscious',
     )
 
 
@@ -33,6 +105,18 @@ def seed_albums():
     db.session.add(album_two)
     db.session.add(album_three)
     db.session.add(album_four)
+    db.session.add(album_five)
+    db.session.add(album_six)
+    db.session.add(album_seven)
+    db.session.add(album_eight)
+    db.session.add(album_nine)
+    db.session.add(album_ten)
+    db.session.add(album_eleven)
+    db.session.add(album_twelve)
+    db.session.add(album_thirteen)
+    db.session.add(album_fourteen)
+    db.session.add(album_fifteen)
+    db.session.add(album_sixteen)
 
     db.session.commit()
 
--- app/seeds/tracks.py
+++ app/seeds/tracks.py
@@ -2,22 +2,40 @@ from app.models import db, Track, environment, SCHEMA
 from sqlalchemy.sql import text
 
 def seed_tracks():
-    demo_track_1 = Track(
-        album_id=1, user_id=4, name='Demo Track 1', duration=184, file_url='file1.url')
-    demo_track_5 = Track(
-        album_id=1, user_id=4, name='Demo Track 5', duration=293, file_url='file5.url')
-    demo_track_2 = Track(
-        album_id=2, user_id=5, name='Demo Track 2', duration=204, file_url='file2.url')
-    demo_track_3 = Track(
-        album_id=3, user_id=6, name='Demo Track 3', duration=192, file_url='file3.url')
-    demo_track_4 = Track(
-        album_id=4, user_id=7, name='Demo Track 4', duration=190, file_url='file4.url')
+    demo_track_1a = Track(
+        album_id=1, user_id=4, name='Demo Track 1a', duration=184, file_url='file1.url')
+    demo_track_1b = Track(
+        album_id=1, user_id=4, name='Demo Track 1b', duration=293, file_url='file5.url')
+    demo_track_1c = Track(
+        album_id=1, user_id=4, name='Demo Track 1c', duration=293, file_url='file5.url')
+    demo_track_2a = Track(
+        album_id=2, user_id=5, name='Demo Track 2a', duration=204, file_url='file2.url')
+    demo_track_2b = Track(
+        album_id=2, user_id=5, name='Demo Track 2b', duration=204, file_url='file2.url')
+    demo_track_2c = Track(
+        album_id=2, user_id=5, name='Demo Track 2c', duration=204, file_url='file2.url')
+    demo_track_3a = Track(
+        album_id=3, user_id=6, name='Demo Track 3a', duration=192, file_url='file3.url')
+    demo_track_3b = Track(
+        album_id=3, user_id=6, name='Demo Track 3b', duration=192, file_url='file3.url')
+    demo_track_3c = Track(
+        album_id=3, user_id=6, name='Demo Track 3c', duration=192, file_url='file3.url')
+    demo_track_4a = Track(
+        album_id=4, user_id=7, name='Demo Track 4a', duration=190, file_url='file4.url')
+    demo_track_4b = Track(
+        album_id=4, user_id=7, name='Demo Track 4b', duration=190, file_url='file4.url')
+    demo_track_4c = Track(
+        album_id=4, user_id=7, name='Demo Track 4b', duration=190, file_url='file4.url')
 
-    db.session.add(demo_track_1)
-    db.session.add(demo_track_2)
-    db.session.add(demo_track_3)
-    db.session.add(demo_track_4)
-    db.session.add(demo_track_5)
+    db.session.add(demo_track_1a)
+    db.session.add(demo_track_1b)
+    db.session.add(demo_track_2a)
+    db.session.add(demo_track_2b)
+    db.session.add(demo_track_3a)
+    db.session.add(demo_track_3b)
+    db.session.add(demo_track_4a)
+    db.session.add(demo_track_4b)
+
 
     db.session.commit()
 

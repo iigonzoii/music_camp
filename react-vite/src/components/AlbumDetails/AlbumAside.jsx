65d6111a12a4b803c9962378a55b391853a0396f
iigonzoii <a.gonzalez418@gmail.com>
2024-08-07 15:35:08 -0500

    aside is displaying data from AlbumUser

--- react-vite/src/components/AlbumDetails/AlbumAside.jsx
+++ react-vite/src/components/AlbumDetails/AlbumAside.jsx
@@ -1,4 +1,4 @@
-import { useEffect} from "react"
+import { useEffect, useState} from "react"
 import { useDispatch, useSelector } from "react-redux"
 import { useParams } from "react-router-dom"
 import { fetchAlbum } from "../../redux/albumReducer"
@@ -6,44 +6,39 @@ import "./AlbumDetails.css"
 
 function AlbumAside() {
     const { albumId } = useParams()
-    // console.log("ALBUMIDDDDDD",albumId)
     const dispatch = useDispatch();
     let album = useSelector(state => state.album.albumDetail);
+    let [isLoaded, setIsLoaded] = useState(false)
     console.log("ALBUM", album)
-
     useEffect(() => {
-        dispatch(fetchAlbum(+albumId))
+        dispatch(fetchAlbum(+albumId)).then(() =>
+            setIsLoaded(true));
     }, [dispatch, albumId]);
-
-    return (
+    if (!album || !album.Album) return
+    return isLoaded && (
         <>
         <div>
             {/* how to access users profile img */}
                         <img className="ADasideImg" src="https://firebasestorage.googleapis.com/v0/b/musiccamp-88aaa.appspot.com/o/musicCampUserProfileImg.jpg?alt=media&token=949d5249-d3c3-4e79-a385-4f3e0774c6bc" />
-                        <div>Artist/Band Name</div>
+                        {/* <div>Artist/Band Name</div> */}
+                        <div>{album.Album.band}</div>
+
                         <div>Artist City</div>
                         <div>Artist Country</div>
                     </div>
                     <div>
                     <div>Artist Website</div>
                     <div>Discography</div>
+
                     </div>
-                        {/* map function to go over album details and build this, include the div above and below img */}
-                    <div>
-                        <img className="ADasideDiscoImgs" src="https://firebasestorage.googleapis.com/v0/b/musiccamp-88aaa.appspot.com/o/musicCampElectronicAlbumCover.jpg?alt=media&token=3f64c61d-5852-4c61-a080-7736041cc47d"/>
-                        <div>Album Title</div>
-                        <div>Album Date</div>
-                    </div>
-                    <div>
-                        <img className="ADasideDiscoImgs" src="https://firebasestorage.googleapis.com/v0/b/musiccamp-88aaa.appspot.com/o/musicCampElectronicAlbumCover.jpg?alt=media&token=3f64c61d-5852-4c61-a080-7736041cc47d"/>
-                        <div>Album Title</div>
-                        <div>Album Date</div>
-                    </div>
-                    <div>
-                        <img className="ADasideDiscoImgs" src="https://firebasestorage.googleapis.com/v0/b/musiccamp-88aaa.appspot.com/o/musicCampElectronicAlbumCover.jpg?alt=media&token=3f64c61d-5852-4c61-a080-7736041cc47d"/>
-                        <div>Album Title</div>
-                        <div>Album Date</div>
-                    </div>
+                    {album && album.UserAlbums.map((album, index) => (
+                    <div key={index}>
+                    <img className="ADasideDiscoImgs" src={album.cover_image_url}/>
+                    <div>{album.title}</div>
+                    <div>{`Released ${album.created_at.split(" ")[2]} ${album.created_at.split(" ")[3]} `}</div>
+
+                </div>
+                    ))}
         </>
     )
 }
